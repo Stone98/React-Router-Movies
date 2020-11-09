5c0d152db7b6807f0f61babe780f6ddbeaf8e304
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2020-11-09 16:28:22 -0700

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Route, Switch } from "react-router-dom";
+import { Route, useParams, Switch } from "react-router-dom";
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
@@ -29,18 +29,21 @@ export default function App() {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
+  const { movieID } = useParams();
+
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
       <Switch>
-        <Route path={'/movie-list/:movieID'}>
+        <Route path={'/movies/:movieID'}>
           <Movie />
         </Route>
         <Route path='/'>
           <MovieList movies={movieList} />
         </Route>
       </Switch>
+
     </div>
   );
 }
